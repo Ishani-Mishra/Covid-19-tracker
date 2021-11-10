b03c0967aacf159d24260e340d60d72eca58a327
Ishani Mishra <ishani1407@gmail.com>
2021-11-10 13:44:50 +0530

    covid tracker

---
+++ src/containers/Global/Global.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Global.css'
+
+ class Upper extends Component {
+    render() {
+        return (
+            <div className="Global">
+               <h1 className="heading">Covid-19 Tracker</h1>
+               
+            </div>
+        )
+    }
+}
+
+export default Upper
